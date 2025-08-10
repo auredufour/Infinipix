dc99f25df7dd7a93a04e8d8f18e175c53f21a240
auredufour <adufour@wealthsimple.com>
2025-08-10 10:48:18 -0400

    feat: base setup for style components

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,19 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: [
+        [
+          'babel-plugin-styled-components',
+          {
+            displayName: true,
+            fileName: false,
+            minify: true,
+            pure: true,
+          },
+        ],
+      ],
+    },
+  })],
 })
