88f0c15cf9e3a76f9f092460239560cc7e4e4867
auredufour <adufour@wealthsimple.com>
2025-08-13 01:27:05 -0400

    fix: avatar

--- src/components/shared/avatar/avatar.types.ts
+++ src/components/shared/avatar/avatar.types.ts
@@ -1,7 +1,8 @@
 import type { HTMLAttributes } from 'react'
 
 export interface DSAvatarProps extends HTMLAttributes<HTMLDivElement> {
+  alt?: string
   name?: string
-  size?: 'small' | 'medium' | 'large' | number
+  size?: 'small' | 'medium' | 'large'
   src?: string
 }
