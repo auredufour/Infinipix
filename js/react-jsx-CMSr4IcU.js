71c85406d12075f03dce617515729e82f89ba8f0
auredufour <adufour@wealthsimple.com>
2025-08-14 01:57:47 -0400

    Updates

---
+++ js/react-jsx-CMSr4IcU.js
@@ -0,0 +1,1 @@
+import{a as t}from"./react-core-D2g-_gCs.js";var r={exports:{}},e;function s(){return e||(e=1,r.exports=t()),r.exports}var u=s();export{u as j};
---
+++ sw.js
@@ -0,0 +1,41 @@
+// Simple service worker for caching static assets
+const CACHE_NAME = 'infinipix-v1'
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // Font files will be cached automatically
+  // JS/CSS chunks will be cached automatically
+]
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)),
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response
+      }
+
+      return fetch(event.request).then((response) => {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== 'basic') {
+          return response
+        }
+
+        // Clone the response
+        const responseToCache = response.clone()
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache)
+        })
+
+        return response
+      })
+    }),
+  )
+})
