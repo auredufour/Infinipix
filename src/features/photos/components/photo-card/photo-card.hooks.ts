381f6ad2dbe6baa6059d187c012bcce801564e15
auredufour <adufour@wealthsimple.com>
2025-08-14 12:16:52 -0400

    fix: kill complexity

--- src/features/photos/components/photo-card/photo-card.hooks.ts
+++ src/features/photos/components/photo-card/photo-card.hooks.ts
@@ -1,45 +1,6 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback } from 'react'
 
 import { downloadImage } from './photo-card.utils'
-
-const ROOT_MARGIN = '50% 0%'
-
-export const useImageLazyLoading = () => {
-  const [isInView, setIsInView] = useState(false)
-  const imgRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    const element = imgRef.current
-
-    if (!element || isInView) return
-
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            setIsInView(true)
-            console.log(
-              '🔍 IntersectionObserver triggered for element:',
-              element,
-            )
-          }
-        })
-      },
-      {
-        rootMargin: ROOT_MARGIN,
-        threshold: 0,
-      },
-    )
-
-    observer.observe(element)
-
-    return () => {
-      observer.disconnect()
-    }
-  }, [isInView])
-
-  return { imgRef, isInView }
-}
 
 export const useDownloadHandler = (downloadUrl: string, author: string) => {
   return useCallback(
