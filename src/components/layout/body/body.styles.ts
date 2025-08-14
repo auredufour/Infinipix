f43b3c518189a8dd10ed83e6cf316c567ac63132
auredufour <adufour@wealthsimple.com>
2025-08-14 01:48:49 -0400

    fix: padding

--- src/components/layout/body/body.styles.ts
+++ src/components/layout/body/body.styles.ts
@@ -6,8 +6,7 @@ export const SCBodyContainer = styled.main`
   --header-height: 80px;
 
   max-width: ${({ theme }) => theme.spacings['content-max-width']};
-  padding: 0 ${({ theme }) => theme.spacings['page-gutter']}
-    ${({ theme }) => theme.spacings['section-gap']};
+  padding: ${({ theme }) => theme.spacings['section-gap']};
   margin: ${({ theme }) =>
     `calc(var(--header-height) + ${theme.spacings['page-gutter']}) auto 0`};
 `
